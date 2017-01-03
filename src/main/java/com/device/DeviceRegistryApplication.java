c3137ca77509ed7df4dffc5b334421ccb78537a3
Kazim Ulusoy <ulusoykazim@gmail.com>
2017-01-03 21:51:42 +0300

    Device Registry initial project

---
+++ src/main/java/com/device/DeviceRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceRegistryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/device/dto/DeviceDto.java
@@ -0,0 +1,5 @@
+package com.device.dto;
+
+public class DeviceDto {
+
+}
